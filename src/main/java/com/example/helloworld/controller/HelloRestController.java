dcaa7511c022629381e672a4815dbb2dffbd75a1
Snehapal2012 <snehapal2012@gamil.com>
2022-08-28 09:54:41 +0530

    Modified: post request method and pass first and last name as query parameter

--- src/main/java/com/example/helloworld/controller/HelloRestController.java
+++ src/main/java/com/example/helloworld/controller/HelloRestController.java
@@ -1,10 +1,12 @@
 package com.example.helloworld.controller;
 
+import com.example.helloworld.data.Entity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value={"/home","/"},method = RequestMethod.GET)
 public class HelloRestController {
+
     @GetMapping(path="/{name}")
     public String getName(@RequestParam(value = "name", defaultValue = "Sneha") String name){
         return "<h1>Hii "+name+" ! Welcome to BridgeLabz!</h1>";
@@ -12,5 +14,9 @@ public class HelloRestController {
     @RequestMapping(value = {"/{name}/{email}"},method = RequestMethod.GET)
     public String getEmail(@PathVariable String email){
         return "<h1>Your "+email+" ID is required!</h1>";
+    }
+    @PostMapping("/post")
+    public String post(@RequestBody Entity entity){
+        return "Welcome "+entity.getFirstName()+" "+entity.getLastName()+"!!!";
     }
 }
---
+++ src/main/java/com/example/helloworld/data/Entity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld.data;
+
+public class Entity {
+    String firstName;
+    String lastName;
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+
+}
